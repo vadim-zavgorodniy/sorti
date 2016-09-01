af0ddb604ac92f0cecac5dbbd219ff4af5dd7333
Vadim Zavgorodniy <vadim@telemediclab.ru>
2016-09-01 14:41:07 +0300

    Функции оформлены в класс

--- generate/main.cpp
+++ generate/main.cpp
@@ -12,37 +12,54 @@
 
 #define DELIMITER ". "
 
-std::vector<char> CHAR_TABLE;
+class DataGeneragor {
+private:
+  std::vector<char> charTable;
+
+public:
+  struct Statistics {
+    // row count
+    int count;
+    // string repeats count
+    int repeats;
+  };
+
+protected:
+  void initCharTable();
+  std::string generateString(size_t maxLength);
+public:
+  Statistics generate(std::ostream& ostr, size_t maxSize);
+};
 
 //============================================================
-void initCharTable() {
-  CHAR_TABLE.clear();
-  CHAR_TABLE.push_back(32); // space
-  CHAR_TABLE.push_back(44); // ,
-  CHAR_TABLE.push_back(46); // .
+void DataGeneragor::initCharTable() {
+  charTable.clear();
+  charTable.push_back(32); // space
+  charTable.push_back(44); // ,
+  charTable.push_back(46); // .
   // digits and : ;
   for (int i = 48; i < 60; ++i) {
-    CHAR_TABLE.push_back(i);
+    charTable.push_back(i);
   }
 
   // digits and : ;
   for (int i = 48; i <= 59; ++i) {
-    CHAR_TABLE.push_back(i);
+    charTable.push_back(i);
   }
 
   // capital letters
   for (int i = 65; i <= 90; ++i) {
-    CHAR_TABLE.push_back(i);
+    charTable.push_back(i);
   }
 
   // small letters
   for (int i = 97; i <= 122; ++i) {
-    CHAR_TABLE.push_back(i);
+    charTable.push_back(i);
   }
 }
 
 //============================================================
-std::string generateString(size_t maxLength) {
+std::string DataGeneragor::generateString(size_t maxLength) {
   // assume that min maxLenth is 10
   const int MIN_BLOCK = 10;
   if (maxLength < MIN_BLOCK) {
@@ -50,7 +67,7 @@ std::string generateString(size_t maxLength) {
   }
 
   // actual string size
-  size_t len = rand() % maxLength;
+  size_t len = rand() % (maxLength-MIN_BLOCK) + MIN_BLOCK;
 
   std::string str;
 
@@ -59,10 +76,10 @@ std::string generateString(size_t maxLength) {
     char buf[MIN_BLOCK + 1];
     memset(buf, '\0', MIN_BLOCK+1);
     // table offset
-    int offset = rand() % (CHAR_TABLE.size() - MIN_BLOCK);
+    int offset = rand() % (charTable.size() - MIN_BLOCK);
 
     // copy blocks to result string
-    strncpy(buf, &CHAR_TABLE[offset], MIN_BLOCK);
+    strncpy(buf, &charTable[offset], MIN_BLOCK);
     str += buf;
   }
 
@@ -70,18 +87,19 @@ std::string generateString(size_t maxLength) {
 }
 
 //============================================================
-void generate(std::ostream& ostr, size_t maxSize) {
+DataGeneragor::Statistics DataGeneragor::generate(std::ostream& ostr, size_t maxSize) {
   initCharTable();
+
+  Statistics stat;
+  stat.count = 0;
+  stat.repeats = 0;
 
   srand(time(NULL));
   // file size
   size_t size = 0;
-  // row count
-  long count = 0;
   // skip amount before repeat string update
   int repeatUpdate = 50;
   std::string repeatStr = "";
-  int repeats = 0;
 
   // while result size less then maxSize in kB
   while (size / 1024 < maxSize) {
@@ -94,11 +112,11 @@ void generate(std::ostream& ostr, size_t maxSize) {
     std::string str = "";
 
     // 2 times often we try to repeat string
-    if (count % (repeatUpdate / 2) == 0) {
+    if (stat.count % (repeatUpdate / 2) == 0) {
       if (rand() % 2 == 1) {
         if (!repeatStr.empty()) {
           str = repeatStr;
-          repeats++;
+          stat.repeats++;
         }
       }
     }
@@ -116,9 +134,9 @@ void generate(std::ostream& ostr, size_t maxSize) {
     // file size
     size += res.size();
     // record count
-    ++count;
+    stat.count++;
 
-    if (count % repeatUpdate == 0) {
+    if (stat.count % repeatUpdate == 0) {
       // shell we remember it?
       if (rand() % 2 == 1) {
         repeatStr = str;
@@ -126,8 +144,7 @@ void generate(std::ostream& ostr, size_t maxSize) {
     }
   }
 
-  std::cout << "Rows count: " << count << std::endl;
-  std::cout << "Repeats count: " << repeats << std::endl;
+  return stat;
 }
 
 //============================================================
@@ -135,9 +152,15 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Start." << std::endl;
   try {
-    std::ofstream ostr("gen_data.txt");
-    generate(ostr, 1024*1024);
+    std::ofstream ostr("gen_data1.txt");
+
+    DataGeneragor dgen;
+    DataGeneragor::Statistics stat = dgen.generate(ostr, 1024*1024);
     ostr.close();
+
+    std::cout << "Rows count: " << stat.count << std::endl;
+    std::cout << "Repeats count: " << stat.repeats << std::endl;
+
     std::cout << "Finish." << std::endl;
   }
   catch (std::exception e) {
